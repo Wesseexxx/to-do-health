4de868f9b40a877c55ada159962e2cf80aca216a
Eduardo Ferreira de Mattos <eduardofermattos2013@gmail.com>
2022-12-05 00:39:43 -0300

    Estilização da página Start

--- src/pages/Start/index.jsx
+++ src/pages/Start/index.jsx
@@ -1,13 +1,16 @@
 import react from 'react'
-import { View, ScrollView, Text, Image } from 'react-native'
+import { View, ScrollView, Text, Image, StyleSheet } from 'react-native'
 
 export default function Start() {
   return (
-    <View>
+    <View style={Styles.conteirner}>
       <ScrollView showsVerticalScrollIndicator={false}>
-        <View>
-          <Image source={require('../../assets/icons/logo3.png')} />
-          <Text>
+        <View style={{ alingItems: 'center' }}>
+          <Image
+            source={require('../../assets/icons/logo3.png')}
+            style={styles.logo}
+          />
+          <Text style={styles.description}>
             Vamos transformar sua vida {'\n'} em um jogo, buscando sempre {'\n'}{' '}
             o melhor nível.
           </Text>
@@ -16,3 +19,24 @@ export default function Start() {
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundcolor: 'rgba(21, 21, 21, 0.96)'
+  },
+
+  logo: {
+    width: 300,
+    height: 60,
+    marginTop: 60,
+    marginBottom: 20
+  },
+
+  description: {
+    color: '#FFFFFF',
+    fontSize: 20,
+    textAlign: 'center',
+    marginVertical: 60
+  }
+})
